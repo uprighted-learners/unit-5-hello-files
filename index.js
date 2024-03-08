8200ed70784fdb4cb7faef1c4c6e0355523aaadd
Ben Anderson <andersjbe@gmail.com>
2024-03-08 11:43:58 -0500

    Completed Solution

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/example.html`);
+});
+
+app.get("/about", (request, response) => {
+  response.sendFile(`${__dirname}/public/about.html`);
+});
+
+app.get("/contact", (request, response) => {
+  response.sendFile(`${__dirname}/public/contact.html`);
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
